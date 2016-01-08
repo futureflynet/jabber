b7f6b99d6644558ed465a1e0007735bc9b974001
Teemu Harju <teemu.harju@gmail.com>
2016-01-08 12:59:38 +0200

    Added exml to apps list.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Jabber.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :exml],
      mod: {Jabber, []}]
   end
 
