45120f719b5ffc86e10df60c3ca3933ba714cba0
Teemu Harju <teemu.harju@gmail.com>
2016-01-12 10:00:36 +0200

    Create message receipts.

--- lib/jabber/stanza/message.ex
+++ lib/jabber/stanza/message.ex
@@ -1,7 +1,16 @@
 defmodule Jabber.Stanza.Message do
   
   use Jabber.Xml
+
+  alias Jabber.Stanza.Message
   
   defstruct(id: nil, to: nil, from: nil, type: "chat",
             body: nil, thread: nil, attrs: [], children: [])
+
+  def receipt(msg) do
+    %Message{to: msg.from, from: msg.to, id: msg.id,
+             children: [xmlel(name: "received",
+                              attrs: [{"id", msg.id}, {"xmlns", "urn:xmpp:receipts"}])]}
+  end
+  
 end
