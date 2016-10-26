13fb9e55faf4f283c19b2963a14e23c0ad329257
Teemu Tuulari <ttuulari@gmail.com>
2016-10-26 14:03:01 +0300

    Extract body from message.

--- lib/jabber/stanza.ex
+++ lib/jabber/stanza.ex
@@ -6,7 +6,7 @@ defmodule Jabber.Stanza do
   alias Jabber.Stanza.Iq
   alias Jabber.Stanza.Message
   alias Jabber.Stanza.Presence
-    
+
   def stream_start(to, xmlns) do
     xmlstreamstart(
       name: "stream:stream",
@@ -17,25 +17,26 @@ defmodule Jabber.Stanza do
   def stream_end do
     xmlstreamend(name: "stream:stream")
   end
-  
+
   def new(xmlel(name: "message", attrs: attrs, children: children) = xml) do
     {"id", id}       = List.keyfind(attrs, "id", 0, {"id", nil})
     {"to", to}       = List.keyfind(attrs, "to", 0, {"to", nil})
     {"from", from}   = List.keyfind(attrs, "from", 0, {"from", nil})
     {"type", type}   = List.keyfind(attrs, "type", 0, {"type", "normal"})
-    
+
     attrs = List.keydelete(attrs, "id", 0)
     attrs = List.keydelete(attrs, "to", 0)
     attrs = List.keydelete(attrs, "from", 0)
     attrs = List.keydelete(attrs, "type", 0)
-    
+
     body   = get_child(xml, "body") |> get_cdata
-    thread = get_child(xml, "thread") |> get_cdata 
-    
+    nick   = get_child(xml, "nick") |> get_cdata
+    thread = get_child(xml, "thread") |> get_cdata
+
     %Message{id: id, to: Jid.new(to), from: Jid.new(from), type: type, body: body,
-             thread: thread, attrs: attrs, children: children}
+             thread: thread, attrs: attrs, children: children, nick: nick}
   end
-  
+
   def new(xmlel(name: "presence", attrs: attrs, children: children)) do
     {"to", to}     = List.keyfind(attrs, "to", 0, {"to", nil})
     {"from", from} = List.keyfind(attrs, "from", 0, {"from", nil})
@@ -44,7 +45,7 @@ defmodule Jabber.Stanza do
     attrs = List.keydelete(attrs, "to", 0)
     attrs = List.keydelete(attrs, "from", 0)
     attrs = List.keydelete(attrs, "type", 0)
-    
+
     %Presence{to: Jid.new(to), from: Jid.new(from), type: type,
               attrs: attrs, children: children}
   end
@@ -59,7 +60,7 @@ defmodule Jabber.Stanza do
     attrs = List.keydelete(attrs, "to", 0)
     attrs = List.keydelete(attrs, "from", 0)
     attrs = List.keydelete(attrs, "type", 0)
-    
+
     %Iq{id: id, to: Jid.new(to), from: Jid.new(from), type: type,
         attrs: attrs, children: children}
   end
@@ -68,14 +69,14 @@ defmodule Jabber.Stanza do
     stanza_to_xml(%{stanza | to: to_string(to), from: to_string(from)})
   end
   def to_xml(stanza), do: stanza_to_xml(stanza)
-  
+
   defp stanza_to_xml(%Message{} = msg) do
     attrs = attrs_to_binary(msg.attrs, msg.id, msg.to, msg.from, msg.type)
 
     children = msg.children
     |> add_child("body", msg.body)
     |> add_child("thread", msg.thread)
-    
+
     xmlel(name: "message", attrs: attrs, children: children)
   end
 
@@ -110,5 +111,5 @@ defmodule Jabber.Stanza do
     child_xml = xmlel(name: name, children: [:exml.escape_cdata(content)])
     [child_xml|children]
   end
-  
+
 end
--- lib/jabber/stanza/message.ex
+++ lib/jabber/stanza/message.ex
@@ -1,16 +1,16 @@
 defmodule Jabber.Stanza.Message do
-  
+
   use Jabber.Xml
 
   alias Jabber.Stanza.Message
-  
+
   defstruct(id: nil, to: nil, from: nil, type: "normal",
-            body: nil, thread: nil, attrs: [], children: [])
+            body: nil, thread: nil, attrs: [], children: [], nick: nil)
 
   def receipt(msg) do
     %Message{to: msg.from, from: msg.to, id: msg.id,
              children: [xmlel(name: "received",
                               attrs: [{"id", msg.id}, {"xmlns", "urn:xmpp:receipts"}])]}
   end
-  
+
 end
