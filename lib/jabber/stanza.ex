03e4f46d5319c0b38b367dd7b2678ab46d04baa6
Teemu Tuulari <ttuulari@gmail.com>
2017-01-19 16:06:53 +0200

    Image related field population.

--- lib/jabber/stanza.ex
+++ lib/jabber/stanza.ex
@@ -37,11 +37,14 @@ defmodule Jabber.Stanza do
     move        = get_child(xml, "animationMove") |> get_cdata
     target      = get_child(xml, "animationTargetId") |> get_cdata
     target_nick = get_child(xml, "animationTargetNick") |> get_cdata
+    width       = get_child(xml, "width") |> get_cdata
+    height      = get_child(xml, "height") |> get_cdata
+    url         = get_child(xml, "url") |> get_cdata
 
     %Message{id: id, to: Jid.new(to), from: Jid.new(from), type: type, body: body,
              thread: thread, attrs: attrs, children: children, nick: nick,
              animationMove: move, animationTargetId: target, animationTargetNick: target_nick,
-             updateNotification: notification}
+             updateNotification: notification, imageUrl: url, imageWidth: width, imageHeight: height}
   end
 
   def new(xmlel(name: "presence", attrs: attrs, children: children)) do
--- lib/jabber/stanza/message.ex
+++ lib/jabber/stanza/message.ex
@@ -5,12 +5,12 @@ defmodule Jabber.Stanza.Message do
   alias Jabber.Stanza.Message
 
   defstruct(id: nil, to: nil, from: nil, type: "normal", body: nil, thread: nil, attrs: [], children: [],
-            nick: nil, animationMove: nil, animationTargetId: nil, animationTargetNick: nil, updateNotification: nil)
+            nick: nil, animationMove: nil, animationTargetId: nil, animationTargetNick: nil, updateNotification: nil,
+            imageUrl: nil, imageWidth: nil, imageHeight: nil)
 
   def receipt(msg) do
     %Message{to: msg.from, from: msg.to, id: msg.id,
              children: [xmlel(name: "received",
                               attrs: [{"id", msg.id}, {"xmlns", "urn:xmpp:receipts"}])]}
   end
-
 end
